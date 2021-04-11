b6feedf69a3fb72bbfc8545174a4b7735eef41a2
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2021-04-12 02:34:44 +0300

    post method

--- src/serve.rs
+++ src/serve.rs
@@ -5,7 +5,7 @@ use async_std::sync::Arc;
 use tide::{Body, Request, Response, StatusCode};
 use uuid::Uuid;
 
-async fn put(req: Request<Arc<State>>) -> tide::Result<String> {
+async fn post(req: Request<Arc<State>>) -> tide::Result<String> {
     let name = Uuid::new_v4().to_string();
     let path = req.state().dir_path.join(&name);
     let file = OpenOptions::new()
@@ -15,6 +15,20 @@ async fn put(req: Request<Arc<State>>) -> tide::Result<String> {
         .await?;
     io::copy(req, file).await?;
     Ok(name)
+}
+
+async fn put(req: Request<Arc<State>>) -> tide::Result<Response> {
+    let path = req.state().dir_path.join(req.param("file")?);
+    if path.exists().await {
+        remove_file(&path).await?;
+    }
+    let file = OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(&path)
+        .await?;
+    io::copy(req, file).await?;
+    Ok(Response::new(StatusCode::Ok))
 }
 
 async fn get(req: Request<Arc<State>>) -> tide::Result<Response> {
@@ -34,7 +48,7 @@ async fn delete(req: Request<Arc<State>>) -> tide::Result<Response> {
 pub async fn serve(config: Config) -> Result<(), Box<dyn std::error::Error>> {
     let srv_addr = config.srv_addr.clone();
     let mut app = tide::with_state(Arc::new(State::from_config(config).await?));
-    app.at("/").put(put);
+    app.at("/").post(post);
     app.at(":file").put(put).get(get).delete(delete);
     app.listen(srv_addr).await?;
     Ok(())
